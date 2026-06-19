ffd5330e4916763bfcd66e3212ccd881201b82b2
Baor97 <marc.badia.o@gmail.com>
2026-06-19 22:03:09 +0200

    Update product.py

--- products/product.py
+++ products/product.py
@@ -1,23 +1,30 @@
 from abc import ABC, abstractmethod
-#Write your code here
+from .food_package import FoodPackage, Wrapping, Bottle
 
 class Product(ABC):
     def __init__(self,id:str,name:str,price:float):
       self.id = id
       self.name = name
-      self.price = price     
-    
+      self.price = price
+
     def describe(self):
-        return f"Product - Type: {self.type()}, Name: {self.name}, Id: {self.id} , Price: {self.price} , {self.foodPackage().describe()}."   
-    
+        package = self.foodPackage()
+        return (
+            f"Product - Type: {self.type()}, Name:{self.name}, "
+            f"Id:{self.id}, Price: {self.price}, "
+            f"{package.describe()}"
+        )    
+
     @abstractmethod
     def type(self) -> str:
         pass
+        
     @abstractmethod
     def foodPackage(self)->FoodPackage:
         pass  
 
 class Hamburger(Product):
+
     def __init__(self,id:str,name:str,price:float):
         super().__init__(id,name,price)
     def type(self) -> str:
@@ -26,13 +33,24 @@ class Hamburger(Product):
         return Wrapping()
         
 class Soda(Product):
-    #Write your code here
-    pass
+    
+    def type(self):
+        return "Soda"
+    def foodPackage(self):
+        return Bottle()
 
 class Drink(Product):
-    #Write your code here
-    pass
+    
+    def type(self):
+        return "Drink"
+
+    def foodPackage(self):
+        return "Bottle"
 
 class HappyMeal(Product):
-    #Write your code here
-    pass
+    
+    def type(self):
+        return "HappyMeal"
+
+    def type(self):
+        return Box()    
